f60e31e4a80a28e0072a00c143fcfc771e549537
David Stephens <stephens.a.david@gmail.com>
2021-06-15 14:54:05 -0500

    completed lecture

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,43 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class MyFirstTest {
+
+    @Test
+    public void isCodeupCodeUp() {
+        String correct = "Codeup";
+        String commonly = "CodeUp";
+
+        assertNotEquals(correct, commonly);
+    }
+
+    @Test
+    public void listsAreDifferent() {
+        List<String> languages = new ArrayList<>();
+        List<String> moreLanguages = new ArrayList<>();
+
+        assertNotSame(languages, moreLanguages);
+    }
+
+    @Test
+    public void arraysAreEqual() {
+        int[] numbers = {1, 2, 3};
+        int[] otherNumbers = new int[3];
+        otherNumbers[0] = 1;
+        otherNumbers[1] = 2;
+        otherNumbers[2] = 3;
+
+        assertArrayEquals(numbers, otherNumbers);
+    }
+
+    @Test
+    public void funWithPHP() {
+        String language = "PHP";
+        assertTrue(language.contains("H")); // use assertTrue for this statement
+        assertFalse(language.contains("J")); // use assertFalse for this statement
+    }
+}
